4205e46623a488fb1d2c6c949926e43beaead0af
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2017-02-23 12:24:07 +0100

    Remove the prefix

--- premake4.lua
+++ premake4.lua
@@ -1,20 +1,3 @@
--- Define the install prefix
-prefix = nil
-    -- Installation under Linux
-    if (os.is('linux')) then
-        prefix = '/usr/local'
-        os.execute('sudo chown -R `whoami` ' .. prefix .. ' && sudo chmod -R 751 ' .. prefix)
-
-    -- Installation under Mac OS X
-    elseif (os.is('macosx')) then
-        prefix = '/usr/local'
-
-    -- Other platforms
-    else
-        print(string.char(27) .. '[31mThe installation is not supported on your platform' .. string.char(27) .. '[0m')
-        os.exit()
-    end
-
 solution 'utilities'
     configurations {'Release', 'Debug'}
     location 'build'
@@ -27,8 +10,8 @@ solution 'utilities'
         files {'source/lodepng.hpp', 'source/html.hpp', 'source/lodepng.cpp', 'source/rainmaker.cpp'}
 
         -- Define the include paths
-        includedirs {path.join(prefix, 'include')}
-        libdirs {path.join(prefix, 'lib')}
+        includedirs {'/usr/local/include'}
+        libdirs {'/usr/local/lib'}
 
         -- Declare the configurations
         configuration 'Release'
@@ -59,8 +42,8 @@ solution 'utilities'
         files {'source/esToCsv.cpp'}
 
         -- Define the include paths
-        includedirs {path.join(prefix, 'include')}
-        libdirs {path.join(prefix, 'lib')}
+        includedirs {'/usr/local/include'}
+        libdirs {'/usr/local/lib'}
 
         -- Declare the configurations
         configuration 'Release'
@@ -91,8 +74,8 @@ solution 'utilities'
         files {'source/datToEs.cpp'}
 
         -- Define the include paths
-        includedirs {path.join(prefix, 'include')}
-        libdirs {path.join(prefix, 'lib')}
+        includedirs {'/usr/local/include'}
+        libdirs {'/usr/local/lib'}
 
         -- Declare the configurations
         configuration 'Release'
@@ -123,8 +106,8 @@ solution 'utilities'
         files {'source/lodepng.hpp', 'source/lodepng.cpp', 'source/shiftTheParadigm.cpp'}
 
         -- Define the include paths
-        includedirs {path.join(prefix, 'include')}
-        libdirs {path.join(prefix, 'lib')}
+        includedirs {'/usr/local/include'}
+        libdirs {'/usr/local/lib'}
 
         -- Declare the configurations
         configuration 'Release'
