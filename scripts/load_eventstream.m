5292d7a4575ce11c363ffc57a89e53622175e49f
Gregor Lenz <lenz.gregor@gmail.com>
2018-05-09 19:30:44 +0200

    skip threshold crossing events, namely grey levels

--- scripts/load_eventstream.m
+++ scripts/load_eventstream.m
@@ -1,7 +1,7 @@
 function event_data = load_eventstream(filename, mirrorX, mirrorY)
 % td_data = load_eventstream(filename, mirrorX=false, mirrorY=false)
 %
-% loads ES files that have been recorded with version 1 of the ES format.
+% loads time differences (but not grey level events) from .es files that have been recorded with version 1 of the ES format.
 % See https://github.com/neuromorphic-paris/event_stream/blob/7ffcca590faa002b48e57f8e75810ceb200888a4/README.md
 
 if ~exist('mirrorX','var')
@@ -26,11 +26,12 @@ end
 data = fread(f, 'uint8');
 
 arraySize = round(length(data)/3) + 1; %if all bytes were event bytes and no overflow or reset bytes
-events = NaN(1, arraySize);
+events = zeros(1, arraySize);
 index = 1;
 tsmask = bin2dec('00011111');
 xmask = bin2dec('00100000');
 pmask = bin2dec('10000000');
+thresholdmask = bin2dec('01000000');
 
 overflow = 0;
 skiploop = 0;
@@ -39,12 +40,12 @@ y = 0;
 t = 0;
 
 for i = 1:length(data)
-    b = bitshift(data(i), 3, 'uint8');
-    
     if skiploop > 0
         skiploop = skiploop - 1;
         continue;
     end
+    
+    b = bitshift(data(i), 3, 'uint8');
     
     if b == 248
         helperbits = bitshift(data(i), -5);
@@ -54,28 +55,30 @@ for i = 1:length(data)
             overflow = overflow + helperbits;
         end
     else 
+        % skip events that encode grey levels
+        if bitand(data(i+2), thresholdmask) ~= thresholdmask
+            t = t + bitand(data(i), tsmask) + overflow * 31;
+            event_data.ts(index) = t;
+            overflow = 0;
 
-        t = t + bitand(data(i), tsmask) + overflow * 31;
-        event_data.ts(index) = t;
-        overflow = 0;
-                
-        x =  bitor(bitshift(data(i), -5), bitshift(data(i+1), 3, 'uint8'));
-        if bitand(data(i+1), xmask) == xmask %account for x[8]
-            x = x + 256;
+            x =  bitor(bitshift(data(i), -5), bitshift(data(i+1), 3, 'uint8'));
+            if bitand(data(i+1), xmask) == xmask %account for x[8]
+                x = x + 256;
+            end
+            event_data.x(index) = x;
+
+            y = bitor(bitshift(data(i+1), -6), bitshift(data(i+2), 2, 'uint8'));
+            event_data.y(index) = y;
+
+            if bitand(data(i+2), pmask) == pmask
+                p = 1;
+            else
+                p = 0;
+            end
+            event_data.p(index) = p;
+
+            index = index + 1;
         end
-        event_data.x(index) = x;
-        
-        y = bitor(bitshift(data(i+1), -6), bitshift(data(i+2), 2, 'uint8'));
-        event_data.y(index) = y;
-          
-        if bitand(data(i+2), pmask) == pmask
-            p = 1;
-        else
-            p = 0;
-        end
-        event_data.p(index) = p;
-        
-        index = index + 1;
         skiploop = 2;
     end
 end
