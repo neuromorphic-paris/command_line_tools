15690f2c85bb100230c9cd013f7a8621bec6b8c9
Alexandre Marcireau <alexandre.marcireau@gmail.com>
2019-04-25 21:56:12 +0200

    Use hash instead of sums in statistics

--- source/statistics.cpp
+++ source/statistics.cpp
@@ -1,7 +1,12 @@
 #include "../third_party/pontella/source/pontella.hpp"
 #include "../third_party/sepia/source/sepia.hpp"
+#include "../third_party/tarsier/source/convert.hpp"
+#include "../third_party/tarsier/source/hash.hpp"
+#include "../third_party/tarsier/source/replicate.hpp"
+#include <iomanip>
+#include <sstream>
 
-/// type_to_string returns a text representation og the type enum.
+/// type_to_string returns a text representation of the type enum.
 std::string type_to_string(sepia::type type) {
     switch (type) {
         case sepia::type::generic:
@@ -14,6 +19,14 @@ std::string type_to_string(sepia::type type) {
             return "color";
     }
     return "unknown";
+}
+
+/// hash_to_string converts a 128 bits hash to a hxadecimal representation.
+std::string hash_to_string(std::pair<uint64_t, uint64_t> hash) {
+    std::stringstream stream;
+    stream << '"' << std::hex << std::get<1>(hash) << std::hex << std::setfill('0') << std::setw(16)
+           << std::get<0>(hash) << '"';
+    return stream.str();
 }
 
 /// properties_to_json converts a list of properties to pretty-printed JSON.
@@ -59,21 +72,36 @@ int main(int argc, char* argv[]) {
                     uint64_t begin_t;
                     uint64_t end_t;
                     std::size_t events = 0;
-                    uint64_t size_sum = 0;
-                    sepia::join_observable<sepia::type::generic>(
-                        sepia::filename_to_ifstream(command.arguments[0]), [&](sepia::generic_event generic_event) {
-                            if (first) {
-                                first = false;
-                                begin_t = generic_event.t;
-                            }
-                            end_t = generic_event.t;
-                            ++events;
-                            size_sum += generic_event.bytes.size();
-                        });
+                    std::string t_hash;
+                    std::string bytes_hash;
+                    {
+                        auto hash = tarsier::make_hash<uint8_t>(
+                            [&](std::pair<uint64_t, uint64_t> hash_value) { bytes_hash = hash_to_string(hash_value); });
+                        sepia::join_observable<sepia::type::generic>(
+                            sepia::filename_to_ifstream(command.arguments[0]),
+                            tarsier::make_replicate<sepia::generic_event>(
+                                [&](sepia::generic_event generic_event) {
+                                    if (first) {
+                                        first = false;
+                                        begin_t = generic_event.t;
+                                    }
+                                    end_t = generic_event.t;
+                                    ++events;
+                                    for (const auto character : generic_event.bytes) {
+                                        hash(character);
+                                    }
+                                },
+                                tarsier::make_convert<sepia::generic_event>(
+                                    [](sepia::generic_event generic_event) -> uint64_t { return generic_event.t; },
+                                    tarsier::make_hash<uint64_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                        t_hash = hash_to_string(hash_value);
+                                    }))));
+                    }
                     properties.emplace_back("begin_t", std::to_string(begin_t));
                     properties.emplace_back("end_t", std::to_string(end_t));
                     properties.emplace_back("events", std::to_string(events));
-                    properties.emplace_back("size_sum", std::to_string(size_sum));
+                    properties.emplace_back("t_hash", t_hash);
+                    properties.emplace_back("bytes_hash", bytes_hash);
                     std::cout << properties_to_json(properties) << std::endl;
                     break;
                 }
@@ -82,29 +110,46 @@ int main(int argc, char* argv[]) {
                     uint64_t begin_t;
                     uint64_t end_t;
                     std::size_t events = 0;
-                    uint64_t x_sum = 0;
-                    uint64_t y_sum = 0;
                     std::size_t increase_events = 0;
+                    std::string t_hash;
+                    std::string x_hash;
+                    std::string y_hash;
                     sepia::join_observable<sepia::type::dvs>(
-                        sepia::filename_to_ifstream(command.arguments[0]), [&](sepia::dvs_event dvs_event) {
-                            if (first) {
-                                first = false;
-                                begin_t = dvs_event.t;
-                            }
-                            end_t = dvs_event.t;
-                            ++events;
-                            x_sum += dvs_event.x;
-                            y_sum += dvs_event.y;
-                            if (dvs_event.is_increase) {
-                                ++increase_events;
-                            }
-                        });
+                        sepia::filename_to_ifstream(command.arguments[0]),
+                        tarsier::make_replicate<sepia::dvs_event>(
+                            [&](sepia::dvs_event dvs_event) {
+                                if (first) {
+                                    first = false;
+                                    begin_t = dvs_event.t;
+                                }
+                                end_t = dvs_event.t;
+                                ++events;
+                                if (dvs_event.is_increase) {
+                                    ++increase_events;
+                                }
+                            },
+                            tarsier::make_convert<sepia::dvs_event>(
+                                [](sepia::dvs_event dvs_event) -> uint64_t { return dvs_event.t; },
+                                tarsier::make_hash<uint64_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    t_hash = hash_to_string(hash_value);
+                                })),
+                            tarsier::make_convert<sepia::dvs_event>(
+                                [](sepia::dvs_event dvs_event) -> uint16_t { return dvs_event.x; },
+                                tarsier::make_hash<uint16_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    x_hash = hash_to_string(hash_value);
+                                })),
+                            tarsier::make_convert<sepia::dvs_event>(
+                                [](sepia::dvs_event dvs_event) -> uint16_t { return dvs_event.y; },
+                                tarsier::make_hash<uint16_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    y_hash = hash_to_string(hash_value);
+                                }))));
                     properties.emplace_back("begin_t", std::to_string(begin_t));
                     properties.emplace_back("end_t", std::to_string(end_t));
                     properties.emplace_back("events", std::to_string(events));
-                    properties.emplace_back("x_sum", std::to_string(x_sum));
-                    properties.emplace_back("y_sum", std::to_string(y_sum));
                     properties.emplace_back("increase_events", std::to_string(increase_events));
+                    properties.emplace_back("t_hash", t_hash);
+                    properties.emplace_back("x_hash", x_hash);
+                    properties.emplace_back("y_hash", y_hash);
                     std::cout << properties_to_json(properties) << std::endl;
                     break;
                 }
@@ -113,40 +158,58 @@ int main(int argc, char* argv[]) {
                     uint64_t begin_t;
                     uint64_t end_t;
                     std::size_t events = 0;
-                    uint64_t x_sum = 0;
-                    uint64_t y_sum = 0;
                     std::size_t dvs_events = 0;
                     std::size_t increase_events = 0;
                     std::size_t second_events = 0;
+                    std::string t_hash;
+                    std::string x_hash;
+                    std::string y_hash;
                     sepia::join_observable<sepia::type::atis>(
-                        sepia::filename_to_ifstream(command.arguments[0]), [&](sepia::atis_event atis_event) {
-                            if (first) {
-                                first = false;
-                                begin_t = atis_event.t;
-                            }
-                            end_t = atis_event.t;
-                            ++events;
-                            x_sum += atis_event.x;
-                            y_sum += atis_event.y;
-                            if (!atis_event.is_threshold_crossing) {
-                                ++dvs_events;
-                            }
-                            if (atis_event.polarity) {
-                                if (atis_event.is_threshold_crossing) {
-                                    ++second_events;
-                                } else {
-                                    ++increase_events;
+                        sepia::filename_to_ifstream(command.arguments[0]),
+                        tarsier::make_replicate<sepia::atis_event>(
+                            [&](sepia::atis_event atis_event) {
+                                if (first) {
+                                    first = false;
+                                    begin_t = atis_event.t;
                                 }
-                            }
-                        });
+                                end_t = atis_event.t;
+                                ++events;
+                                if (!atis_event.is_threshold_crossing) {
+                                    ++dvs_events;
+                                }
+                                if (atis_event.polarity) {
+                                    if (atis_event.is_threshold_crossing) {
+                                        ++second_events;
+                                    } else {
+                                        ++increase_events;
+                                    }
+                                }
+                            },
+                            tarsier::make_convert<sepia::atis_event>(
+                                [](sepia::atis_event atis_event) -> uint64_t { return atis_event.t; },
+                                tarsier::make_hash<uint64_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    t_hash = hash_to_string(hash_value);
+                                })),
+                            tarsier::make_convert<sepia::atis_event>(
+                                [](sepia::atis_event atis_event) -> uint16_t { return atis_event.x; },
+                                tarsier::make_hash<uint16_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    x_hash = hash_to_string(hash_value);
+                                })),
+                            tarsier::make_convert<sepia::atis_event>(
+                                [](sepia::atis_event atis_event) -> uint16_t { return atis_event.y; },
+                                tarsier::make_hash<uint16_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    y_hash = hash_to_string(hash_value);
+                                }))));
                     properties.emplace_back("begin_t", std::to_string(begin_t));
                     properties.emplace_back("end_t", std::to_string(end_t));
                     properties.emplace_back("events", std::to_string(events));
-                    properties.emplace_back("x_sum", std::to_string(x_sum));
-                    properties.emplace_back("y_sum", std::to_string(y_sum));
                     properties.emplace_back("dvs_events", std::to_string(dvs_events));
                     properties.emplace_back("increase_events", std::to_string(increase_events));
                     properties.emplace_back("second_events", std::to_string(second_events));
+                    properties.emplace_back("t_hash", t_hash);
+                    properties.emplace_back("x_hash", x_hash);
+                    properties.emplace_back("y_hash", y_hash);
+
                     std::cout << properties_to_json(properties) << std::endl;
                     break;
                 }
@@ -155,33 +218,62 @@ int main(int argc, char* argv[]) {
                     uint64_t begin_t;
                     uint64_t end_t;
                     std::size_t events = 0;
-                    uint64_t x_sum = 0;
-                    uint64_t y_sum = 0;
-                    uint64_t r_sum = 0;
-                    uint64_t g_sum = 0;
-                    uint64_t b_sum = 0;
+                    std::string t_hash;
+                    std::string x_hash;
+                    std::string y_hash;
+                    std::string r_hash;
+                    std::string g_hash;
+                    std::string b_hash;
                     sepia::join_observable<sepia::type::color>(
-                        sepia::filename_to_ifstream(command.arguments[0]), [&](sepia::color_event color_event) {
-                            if (first) {
-                                first = false;
-                                begin_t = color_event.t;
-                            }
-                            end_t = color_event.t;
-                            ++events;
-                            x_sum += color_event.x;
-                            y_sum += color_event.y;
-                            r_sum += color_event.r;
-                            g_sum += color_event.g;
-                            b_sum += color_event.b;
-                        });
+                        sepia::filename_to_ifstream(command.arguments[0]),
+                        tarsier::make_replicate<sepia::color_event>(
+                            [&](sepia::color_event color_event) {
+                                if (first) {
+                                    first = false;
+                                    begin_t = color_event.t;
+                                }
+                                end_t = color_event.t;
+                                ++events;
+                            },
+                            tarsier::make_convert<sepia::color_event>(
+                                [](sepia::color_event color_event) -> uint64_t { return color_event.t; },
+                                tarsier::make_hash<uint64_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    t_hash = hash_to_string(hash_value);
+                                })),
+                            tarsier::make_convert<sepia::color_event>(
+                                [](sepia::color_event color_event) -> uint16_t { return color_event.x; },
+                                tarsier::make_hash<uint16_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    x_hash = hash_to_string(hash_value);
+                                })),
+                            tarsier::make_convert<sepia::color_event>(
+                                [](sepia::color_event color_event) -> uint16_t { return color_event.y; },
+                                tarsier::make_hash<uint16_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    y_hash = hash_to_string(hash_value);
+                                })),
+                            tarsier::make_convert<sepia::color_event>(
+                                [](sepia::color_event color_event) -> uint8_t { return color_event.r; },
+                                tarsier::make_hash<uint8_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    r_hash = hash_to_string(hash_value);
+                                })),
+                            tarsier::make_convert<sepia::color_event>(
+                                [](sepia::color_event color_event) -> uint8_t { return color_event.g; },
+                                tarsier::make_hash<uint8_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    g_hash = hash_to_string(hash_value);
+                                })),
+                            tarsier::make_convert<sepia::color_event>(
+                                [](sepia::color_event color_event) -> uint8_t { return color_event.b; },
+                                tarsier::make_hash<uint8_t>([&](std::pair<uint64_t, uint64_t> hash_value) {
+                                    b_hash = hash_to_string(hash_value);
+                                }))));
                     properties.emplace_back("begin_t", std::to_string(begin_t));
                     properties.emplace_back("end_t", std::to_string(end_t));
                     properties.emplace_back("events", std::to_string(events));
-                    properties.emplace_back("x_sum", std::to_string(x_sum));
-                    properties.emplace_back("y_sum", std::to_string(y_sum));
-                    properties.emplace_back("r_sum", std::to_string(r_sum));
-                    properties.emplace_back("g_sum", std::to_string(g_sum));
-                    properties.emplace_back("b_sum", std::to_string(r_sum));
+                    properties.emplace_back("t_hash", t_hash);
+                    properties.emplace_back("x_hash", x_hash);
+                    properties.emplace_back("y_hash", y_hash);
+                    properties.emplace_back("r_hash", r_hash);
+                    properties.emplace_back("g_hash", g_hash);
+                    properties.emplace_back("b_hash", b_hash);
                     std::cout << properties_to_json(properties) << std::endl;
                     break;
                 }
